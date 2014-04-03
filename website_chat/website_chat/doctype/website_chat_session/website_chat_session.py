21d6cc24c0946337ec87740a1725ca668eab40e8
Rushabh Mehta <rmehta@gmail.com>
2014-04-03 11:46:57 +0530

    frappe/frappe#478 removed self.doclist

--- website_chat/website_chat/doctype/website_chat_session/website_chat_session.py
+++ website_chat/website_chat/doctype/website_chat_session/website_chat_session.py
@@ -9,7 +9,7 @@ from frappe.model.document import Document
 class WebsiteChatSession(Document):
 	
 	def validate(self):
-		last_message_by = self.doclist.get({"doctype":"Website Chat Message"})
+		last_message_by = self.get("website_chat_messages")
 		
 		if last_message_by:
 			last_message_by = last_message_by and last_message_by[-1].get("owner")
@@ -21,4 +21,4 @@ class WebsiteChatSession(Document):
 			
 	def on_update(self):
 		frappe.cache().delete_value("website-chat-active-sessions")
-		frappe.cache().set_value(self.name, self.doclist)
+		frappe.cache().set_value(self.name, self)
